cfc43a56abc3a17f6520a11bd3afee534889e43f
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-01-31 14:50:58 -0500

    Initial Next.js commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="flex items-center bg-gray-100 dark:bg-gray-800 p-4">
+      Created by Daniel J. Lewis and James Cridland.
+    </footer>
+  );
+}
