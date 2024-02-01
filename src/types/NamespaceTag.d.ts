bba322967e15d8a6b86d1f06d1213501f9fff4a5
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-01-31 22:13:48 -0500

    Extended namespace type

--- src/types/NamespaceTag.d.ts
+++ src/types/NamespaceTag.d.ts
@@ -2,10 +2,12 @@
 
 type NamespaceTag = {
   name: string;
+  label: string;
   tag: string;
   description?: string;
   levels: string[];
   documentationUrl: string;
   examples?: string[];
   exampleUrl?: string;
+  exampleImageUrls?: string[];
 };
