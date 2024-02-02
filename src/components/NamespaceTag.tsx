e19b6cc9da20e7aed850a1931fa4e45ec0f7d57a
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-02-02 17:37:06 -0500

    Added "popular" badge on namespace list page Improved namespace list page buttons

--- src/components/NamespaceTag.tsx
+++ src/components/NamespaceTag.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Badge } from "./ui/badge";
 import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 
 export function NamespaceTag({ tag }: { tag: NamespaceTag }) {
   return (
@@ -12,16 +12,19 @@ export function NamespaceTag({ tag }: { tag: NamespaceTag }) {
           "border-green-300 bg-green-200 dark:border-green-800 dark:bg-green-900",
       )}
     >
-      <h3 className="mt-0 flex items-center gap-1" id={tag.label}>
+      <h3 className="mt-0 flex items-center gap-2" id={tag.label}>
         {tag.label}
+        {tag.popular && (
+          <Badge className="bg-green-700 dark:bg-green-300">Popular!</Badge>
+        )}
       </h3>
       <p>
         <code>{tag.tag}</code>
       </p>
       <p>{tag.description?.short}</p>
-      <Link href={`/podcast-namespace/tags/${tag.slug}`} className="mt-auto">
-        <Button>Details</Button>
-      </Link>
+      <Button asChild className="mr-auto mt-auto">
+        <Link href={`/podcast-namespace/tags/${tag.slug}`}>Details</Link>
+      </Button>
     </div>
   );
 }
