4e2c8e03a81bd194f22c360c974b5c5ffd4639ba
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-02-01 23:48:46 -0500

    Added long descriptions to tags

--- src/components/NamespaceTag.tsx
+++ src/components/NamespaceTag.tsx
@@ -1,29 +1,33 @@
 import Link from "next/link";
 import { Badge } from "./ui/badge";
+import { nl2br } from "react-js-nl2br";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 export function NamespaceTag({ tag }: { tag: NamespaceTag }) {
   return (
-    <>
-      <h2 className="flex items-center gap-1" id={tag.label}>
+    <div className="mb-16">
+      <h3 className="flex items-center gap-1" id={tag.label}>
         <code>{tag.tag}</code>
         {tag.parents.map((parent) => (
           <Badge variant="secondary" key={parent}>
             {parent}
           </Badge>
         ))}
-      </h2>
+      </h3>
       <p>{tag.description?.short}</p>
       <p>
-        {tag.documentationUrl && (
-          <Link href={tag.documentationUrl}>Documentation</Link>
-        )}
-        {/* {tag.exampleUrl && (
-          <>
-            {" "}
-            | <Link href={tag.exampleUrl}>Example</Link>
-          </>
-        )} */}
+        <code>{nl2br(tag.examples?.[0].code)}</code>
       </p>
-    </>
+      <Collapsible>
+        <CollapsibleTrigger>
+          <h4>Details</h4>
+        </CollapsibleTrigger>
+        <CollapsibleContent>{nl2br(tag.description?.long)}</CollapsibleContent>
+      </Collapsible>
+    </div>
   );
 }
