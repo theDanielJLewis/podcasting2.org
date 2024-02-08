febbc32a3a3e4d73d8075ca7b1feb84fe5c16227
James Cridland <james@cridland.net>
2024-02-08 16:12:50 +1000

    Update Footer.tsx
    JRC link to personal website

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -15,12 +15,11 @@ export function Footer() {
           </Link>{" "}
           and{" "}
           <Link
-            href="https://podnews.net"
+            href="https://james.cridland.net/"
             className="transition-all hover:text-slate-300"
           >
             James Cridland
           </Link>
-          .
           <br />
           All content is licensed under{" "}
           <Link
