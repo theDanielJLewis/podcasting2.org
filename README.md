5c1224fba257dc61f528915747ef1c7d1380aacf
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-02-12 18:50:59 -0500

    Started adding more explanations to front page Updated README

--- README.md
+++ README.md
@@ -31,3 +31,17 @@ Then [open http://localhost:3000](http://localhost:3000) in your browser to see 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Open Sans, a custom Google Font.
+
+## Suggesting improvements
+
+Please [report an issue](https://github.com/theDanielJLewis/podcasting2.org/issues) if you find problems in or have suggestions for any specific content or functionality already on Podcasting2.org.
+
+For suggesting new content or features, please [start a discussion](https://github.com/theDanielJLewis/podcasting2.org/discussions).
+
+## Project structure
+
+As a Next.js project, this website relies on a folder-based structure for URL structure. All pages will be generated from a `page.tsx` file and will appear in a folder under `/src/app/` based on its path.
+
+For example, `podcasting2.org/about` is generated from `/src/app/about/page.tsx`.
+
+Currently, all Podcast Namespace information is pulled from `/src/data/podcastNamespaceTags.ts`, while information from apps is fetched from [the Podcast Index repo](https://github.com/Podcastindex-org/web-ui/blob/master/server/data/apps.json).
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,56 +20,54 @@ export default function Home() {
           Making podcasting better for audiences, podcasters, and developers
         </h1>
       </div>
-      <div>
-        <div className="grid gap-8 md:grid-cols-3">
-          <div className="flex flex-col items-start py-3">
-            <Headphones size={48} />
-            <h2 className="my-3 text-2xl">For audiences</h2>
-            <p>
-              Enjoy richer listening and viewing experiences! Podcasting 2.0
-              lets you follow, engage with, support, and share podcasts more
-              easily.
-            </p>
-            <Link href="/apps" className="mt-auto">
-              <Button>Find a better app!</Button>
-            </Link>
-            {/* <LinkContainer to="/apps">
+      <div className="mb-8 grid gap-8 md:grid-cols-3">
+        <div className="flex flex-col items-start py-3">
+          <Headphones size={48} />
+          <h2 className="my-3 text-2xl">For audiences</h2>
+          <p>
+            Enjoy richer listening and viewing experiences! Podcasting 2.0 lets
+            you follow, engage with, support, and share podcasts more easily.
+          </p>
+          <Link href="/apps" className="mt-auto">
+            <Button>Find a better app!</Button>
+          </Link>
+          {/* <LinkContainer to="/apps">
               <Button>Try a better podcast app!</Button>
             </LinkContainer> */}
-          </div>
-          <div className="flex flex-col items-start py-3">
-            <Mic size={48} />
+        </div>
+        <div className="flex flex-col items-start py-3">
+          <Mic size={48} />
 
-            <h2 className="my-3 text-2xl">For podcasters</h2>
-            <p>
-              Podcasting 2.0 offers the new Podcast Namespace to improve your
-              podcast, grow your audience, and even monetize your show!
-            </p>
-            <Link href="/podcasters" className="mt-auto">
-              <Button>Upgrade my podcast!</Button>
-            </Link>
+          <h2 className="my-3 text-2xl">For podcasters</h2>
+          <p>
+            Podcasting 2.0 offers the new Podcast Namespace to improve your
+            podcast, grow your audience, and even monetize your show!
+          </p>
+          <Link href="/podcasters" className="mt-auto">
+            <Button>Upgrade my podcast!</Button>
+          </Link>
 
-            {/* <LinkContainer to="/podcasters">
+          {/* <LinkContainer to="/podcasters">
               <Button>Upgrade to Podcasting 2.0!</Button>
             </LinkContainer> */}
-          </div>
-          <div className="flex flex-col items-start py-3">
-            <Code size={48} />
+        </div>
+        <div className="flex flex-col items-start py-3">
+          <Code size={48} />
 
-            <h2 className="my-3 text-2xl">For developers</h2>
-            <p>
-              Integrate an open, inclusive, always-free podcast catalog API and
-              add Podcasting 2.0 features to your app. Make finding, following,
-              and funding podcasts easier for your users!
-            </p>
-            <Link href="/podcast-namespace" className="mt-auto">
-              <Button>Add the Podcast Namespace!</Button>
-            </Link>
-            {/* <LinkContainer to="/developers">
+          <h2 className="my-3 text-2xl">For developers</h2>
+          <p>
+            Integrate an open, inclusive, always-free podcast catalog API and
+            add Podcasting 2.0 features to your app. Make finding, following,
+            and funding podcasts easier for your users!
+          </p>
+          <Link href="/podcast-namespace" className="mt-auto">
+            <Button>Add the Podcast Namespace!</Button>
+          </Link>
+          {/* <LinkContainer to="/developers">
               <Button>Put Podcasting 2.0 in your app!</Button>
             </LinkContainer> */}
-          </div>
-          {/* <div className="col-span-full flex flex-col items-start py-3">
+        </div>
+        {/* <div className="col-span-full flex flex-col items-start py-3">
             <Lightbulb size={48} />
 
             <h2 className="my-3 text-2xl">For anyone with ideas</h2>
@@ -80,7 +78,75 @@ export default function Home() {
             </p>
             <Button className="mt-auto">Get involved!</Button>
           </div> */}
-        </div>
+      </div>
+      <div className="mb-8">
+        <h2 className="text-2xl">What does Podcasting 2.0 offer?</h2>
+        <p>
+          Podcasting 2.0 is a set of new features and standards that make
+          podcasting better for everyone.
+        </p>
+        <p>
+          Since RSS (&ldquo;really simple syndication&rdquo;) is the core
+          technology that makes podcasting possible, Podcasting 2.0 extends this
+          standard with new RSS tags to enable new features.
+        </p>
+        <p>
+          Some of these new features (like the{" "}
+          <code>&lt;podcast:podroll&gt;</code> tag for recommending other
+          podcasts) can be added to podcast feeds simply by adding the
+          appropriate RSS tags. Many publishing tools and hosting providers make
+          this as simple as filling in additional field.
+        </p>
+        <p>
+          Other features (like using <code>&lt;podcast:value&gt;</code> to
+          accept &ldquo;boostagrams&rdquo; and streaming payments) require
+          additional tools and a few more initial steps.
+        </p>
+      </div>
+      <div className="mb-8">
+        <h2>Where does Podcasting 2.0 work?</h2>
+        <p>
+          Podcasting 2.0 already has various levels of support from{" "}
+          <Link href="/apps">
+            independent modern podcast apps to Apple Podcasts
+          </Link>
+          !
+        </p>
+        <p>
+          To take advantage of Podcasting 2.0 today,{" "}
+          <Link href="/publishing-tools">
+            check which features your hosting provider or publishing tool
+            supports
+          </Link>
+          , or consider switching to one of these providers or tools.
+        </p>
+      </div>
+      <div className="mb-8">
+        <h2>How to upgrade to Podcasting 2.0</h2>
+        <p>
+          Getting the most from Podcasting 2.0 can be summed up with these two
+          steps:
+        </p>
+        <ol className="mb-8 list-decimal pl-8">
+          <li>
+            Start implementing the Podcasting 2.0 features in your own podcast.
+          </li>
+          <li>
+            Encourage your audience to try a better podcast app from{" "}
+            <Link
+              href="/apps"
+              className="text-amber-600 transition-all hover:text-amber-700 hover:underline dark:text-amber-400 dark:hover:text-amber-500"
+            >
+              our list at Podcasting2.org/apps
+            </Link>
+            .
+          </li>
+        </ol>
+        <p>
+          <Link href="/get-involved">
+            <Button>Learn how to get involved</Button>
+          </Link>
+        </p>
       </div>
     </>
   );
