d8ffeb3f7ab6a2ef639ddab0fd9e1d64c1f5eaf7
James Cridland <james@cridland.net>
2024-03-03 20:55:37 +1000

    Update page.tsx

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -19,13 +19,17 @@ export default function MarketingPage() {
       </HeroHeader>
       <h2>Audio trailers and IDs</h2>
       <p>
-        Please feel free to use these IDs in your show.
+        Please feel free to use these IDs in your show. Right-click them to save the audio.
         If you have room to credit the voice talent,
         please do so in the show notes or the show credits.
         You can also thank them with a boost to mattcundill@getalby.com</p>
 
       <p>
-<div dangerouslySetInnerHTML={{__html: '<audio controls src="https://podcasting2.org/audio/cundill-promo.mp3"></audio>'}}></div>
+<div dangerouslySetInnerHTML={{__html: '<audio controls src="https://podcasting2.org/audio/cundill-promo.mp3"></audio><br>Male Voice: <a href="https://www.mattcundillvoice.com/">Matt Cundill</a>'}}></div>
+      </p>
+
+     <p>
+<div dangerouslySetInnerHTML={{__html: '<audio controls src="https://podcasting2.org/audio/ivision-promo.mp3"></audio><br>Female Voice: <a href="https://www.ivisonvoice.com/">Mary Anne Ivison</a>'}}></div>
       </p>
 
       <h2>Or use your power as a podcaster</h2>
