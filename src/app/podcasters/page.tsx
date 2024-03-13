685c8b16c035a6951119aef16643d3b5fcba2395
James Cridland <james@cridland.net>
2024-03-13 13:21:15 +1000

    Update page.tsx
    Rewritten a bit to de-techify. The namespace gets much less prominence. Also highlighted transcripts because Apple.

--- src/app/podcasters/page.tsx
+++ src/app/podcasters/page.tsx
@@ -14,26 +14,28 @@ export default function PodcastersPage() {
       <HeroHeader>
         <h1>Podcasting 2.0 is for podcasters!</h1>
         <span>
-          We believe podcasts deserve an open, independent podcasting ecosystem.
-          We created Podcasting 2.0 to preserve, protect, and <em>extend</em>{" "}
+          Podcasts deserve an open, independent podcasting ecosystem. 
+          Podcasting 2.0 was created to preserve, protect, and <em>extend</em>{" "}
           that ecosystem.
         </span>
       </HeroHeader>
-      <h2>Upgrade to the Podcast Namespace</h2>
+      <h2>Upgrade your podcast</h2>
       <p>
-        Really Simple Syndication (RSS) is the foundation for open podcasting.
-        But podcast feeds have lacked innovation for years, due in large part to
-        leaving it in the hands of large corporatations to innovate&mdash;and
-        regularly being disappointed.
+        Every good podcast starts with an RSS feed - the foundation for open podcasting.
+        But, open podcast feeds have lacked innovation for years: much of the innovation
+        that podcasting has seen recently has been from large corporations building new
+        tools for their enclosed ecosystems - which don’t benefit open podcasting, and
+        make it harder for podcasters to get the best for their listeners on all platforms.
       </p>
       <p>
         Podcasting 2.0 changes that by finally bringing fresh innovation to
-        podcasting&rsquo;s core technology of RSS. We do this through the
-        Podcast Namespace: a new standard for RSS feeds that bring multiple
-        benefits for audiences, podcasters, and developers.
+        podcasting&rsquo;s core technology of RSS. RSS has always been able to be extended
+        to add new features; and through a new “namespace” - a set of standard features for
+        RSS - we can bring multiple benefits for audiences, podcasters, and developers.
       </p>
       <h3>Podcast Namespace benefits</h3>
       <ul className="list-disc pl-8">
+        <li>Add transcripts that the creator controls, adding accessibility for all</li>
         <li>
           Add and edit interactive chapters—<em>without</em> re-uploading your
           media file
@@ -52,17 +54,19 @@ export default function PodcastersPage() {
       <h2>How to get started</h2>
       <ol className="list-decimal pl-8">
         <li>
-          Ensure your podcast is in the Podcast Index, and{" "}
+          Ensure your podcast is in the Podcast Index. 
           <Link href="https://podcastindex.org/add" target="_blank">
-            add your podcast if it&rsquo;s not included
+            Add your podcast if it&rsquo;s not included
           </Link>
-          .
+          . The Podcast Index in the podcast directory used by most new
+          podcast apps, and this will ensure your podcast is easy to find.
         </li>
         <li>
-          Check with your podcast-feed-publishing tool for support of the
-          Podcasting 2.0 features from the{" "}
-          <Link href="/podcast-namespace">new Podcast Namespace</Link>. You
-          might even want to switch providers in order to get the new features.
+          Check with your podcast hosting company, or your RSS publishing tool,
+          whether it supports Podcasting 2.0 features from the{" "}
+          <Link href="/podcast-namespace">new podcast namespace</Link>. You
+          might even want to switch providers in order to get the new features
+          you're interested in using.
         </li>
         <li>
           Prepare your podcast to receive streaming Bitcoin/Satoshis and
@@ -83,8 +87,7 @@ export default function PodcastersPage() {
         <li>
           <a href="https://podcastindex.social/invite/hfcQYbjq" target="_blank">
             Participate in PodcastIndex.social
-          </a>{" "}
-          to share your ideas and feedback.
+          </a>, a social network for podcasters at the cutting edge, to share your ideas and feedback.
         </li>
         <li>
           Go deeper with our <Link href="/developers">developer resources</Link>
