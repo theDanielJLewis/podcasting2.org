e61464ed5874f7183bfc4587fd1ff9bddcb69f86
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2024-04-23 09:00:34 -0400

    Fixed build error

--- src/components/ui/Article.tsx
+++ src/components/ui/Article.tsx
@@ -1,10 +1,18 @@
 import { cn } from "@/lib/utils";
 
-export function Article({ children, className, ...props }) {
+export function Article({
+  children,
+  className,
+  ...props
+}: {
+  children: React.ReactNode;
+  className: string;
+  props?: any;
+}) {
   return (
     <article
       {...props}
-      className={cn("prose dark:prose-invert max-w-none", className)}
+      className={cn("prose max-w-none dark:prose-invert", className)}
     >
       {children}
     </article>
